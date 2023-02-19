652604dad505dfa69340cac4d5a76c3b2f2fa30d
biiggman <bryanbenham46@gmail.com>
2023-02-18 20:30:45 -0600

    firebase installed

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:aacademic/camera_page.dart';
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
   runApp(const MyApp());
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }
 
 class MyApp extends StatelessWidget {
