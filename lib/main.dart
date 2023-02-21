4377cddbefb2a58cd28c12324ab8515637c98a7a
Kenneth Gregory <kennethsgregory@gmail.com>
2023-02-21 15:19:07 -0600

    modified:   lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-    home: MyHomePage(title: 'AACademic Demo',),
+      home: MyHomePage(
+        title: 'AACademic Demo',
+      ),
     );
   }
 }
@@ -36,10 +38,36 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  int _selectedIndex = 0;
+
+  static const TextStyle optionStyle = TextStyle(
+      fontSize: 30,
+      fontWeight:
+          FontWeight.bold); //define font/size for text refernced in List
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: List',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Dashboard',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: Settings',
+      style: optionStyle,
+    ),
+  ];
 
   void _incrementCounter() {
     setState(() {
       _counter++;
+    });
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
     });
   }
 
@@ -53,6 +81,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            _widgetOptions.elementAt(_selectedIndex),
             const Text(
               'You have pushed the button this many times:',
             ),
@@ -60,12 +89,36 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
-            ElevatedButton(onPressed: () async {
-              await availableCameras().then((value) => Navigator.push(context,
-              MaterialPageRoute(builder: (_) => CameraPage(cameras: value))));
-            }, child: const Text('Camera'),),
+            ElevatedButton(
+              onPressed: () async {
+                await availableCameras().then((value) => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (_) => CameraPage(cameras: value))));
+              },
+              child: const Text('Camera'),
+            ),
           ],
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: 'Menu',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.dashboard),
+            label: 'Imageboard',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800], //color options
+        onTap: _onItemTapped,
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
